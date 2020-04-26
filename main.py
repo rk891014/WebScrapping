e8235335e97498b58ca1f1800b7c89b0ea5d0bd6
Rohit12 <57087470+rk891014@users.noreply.github.com>
2020-04-27 03:48:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,27 @@ import requests as requests
 import bs4
 import io
 
-url = "https://finance.yahoo.com/quote/GC=F?p=GC=F"
-data = requests.get(url)
-soup = bs4.BeautifulSoup(data.text, "html.parser")
+url="https://www.youtube.com/channel/UCa5UDzFzpIjJ1VSxCSlQP_g/videos"
+data=requests.get(url)
 
-for gold in :
-    print(gold)
+
+soup=bs4.BeautifulSoup(data.text,"html.parser")
+formatted_text=soup.prettify()
+filename="temp.html"
+# print(formatted_text)
+
+
+with io.open(filename, 'a',encoding="utf-8") as the_file:
+    the_file.write(formatted_text)
+
+
+
+# this is for finding all para in page
+
+for para in soup.findAll('p'):
+    print(para.text)
+
+# for link in soup.findAll('a'):
+#     link=link.get('href')
+#     # if link[0:3]=="/wa":
+#     print(link)
